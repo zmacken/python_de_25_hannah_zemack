12d7b14824172db5e9fea12809b14f214802281f
Hannah Zemack <hannah.zemack@gmail.com>
2025-10-23 10:17:21 +0200

    n

--- code_alongs/lesson23-10_walktrough.py
+++ code_alongs/lesson23-10_walktrough.py
@@ -1,7 +1,22 @@
+from numbers import Number
+
 class UnitConverter:
     def __init__(self, value: int | float): #| means or
         self.value = value
 
-unit_converter = UnitConverter(5)
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, Number):
+            raise TypeError ('input a value that is a number')
+        if not value >= 0:
+            raise ValueError ('number should be larger or equal to 0')
+        self._value = value
+    
+
+unit_converter = UnitConverter(3)
 
 print(f'{unit_converter.value = }')
